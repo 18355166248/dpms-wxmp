5ab4559d88bd005f6b52cc9bfb0a0a13bba2399c
jianglang <643546122@qq.com>
2020-07-23 15:26:52 +0800

    feat: 初始化lerna提交钩子

---
+++ packages/MP-C/src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="content">
+    <van-cell title="单元格" value="内容" />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+    }
+  },
+  onLoad() {},
+  methods: {},
+  components: {},
+}
+</script>
+
+<style>
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
