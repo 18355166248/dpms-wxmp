bdc0b26065ade632990b10e0de9d7ff17887548c
jianglang <643546122@qq.com>
2020-07-24 11:11:44 +0800

    feat: 设置公共配置

--- packages/MP-C/src/main.js
+++ packages/MP-C/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import { toastUtil } from 'mpcommon'
 
 Vue.config.productionTip = false
+
+Vue.prototype = Object.assign(Vue.prototype, toastUtil)
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+  ...App,
 })
 app.$mount()
---
+++ packages/MP-common/index.js
@@ -0,0 +1,1 @@
+export { default as toastUtil } from "./utils/toast.js";
