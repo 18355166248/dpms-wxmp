80d5ad6cd0bff9a205255a0653e14ad4b1a08607
jianglang <643546122@qq.com>
2020-07-24 13:38:45 +0800

    feat: 初始化部分公共方法

--- packages/MP-B/src/main.js
+++ packages/MP-B/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-import { toastUtil } from 'mpcommon'
+import { toastUtil, flyUtil, commonUtil } from 'mpcommon'
 
 Vue.config.productionTip = false
 
-Vue.prototype = Object.assign(Vue.prototype, toastUtil)
+Vue.prototype = Object.assign(Vue.prototype, commonUtil, toastUtil, { flyUtil })
 
 App.mpType = 'app'
 
