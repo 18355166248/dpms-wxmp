19c1d1bd90e03f4fc8c2caa29b653c7d625d03c3
xiubin.li <103432847@qq.com>
2020-07-28 11:02:18 +0800

    feat: 添加登录页，选择诊所组件

--- packages/MP-B/src/components/selectMedicalInstitution/selectMedicalInstitution.vue
+++ packages/MP-B/src/components/selectMedicalInstitution/selectMedicalInstitution.vue
@@ -1,76 +1,223 @@
-<template>
-  <div class="content">
-    <tki-tree
-      ref="tkitree"
-      idKey="id"
-      rangeKey="name"
-      selectParent
-      :range="list"
-      @confirm="treeConfirm"
-    ></tki-tree>
-  </div>
+<template xlang="wxml">
+  <view class="tki-tree">
+    <view class="tki-tree-mask" :class="{ show: showTree }" @tap="hide"></view>
+    <view class="tki-tree-cnt" :class="{ show: showTree }">
+      <view class="tki-tree-bar">
+        {{ title }}
+      </view>
+      <view class="tki-tree-view">
+        <scroll-view class="tki-tree-view-sc" :scroll-y="true">
+          <block v-for="(item, index) in treeList" :key="index">
+            <view
+              class="tki-tree-item"
+              :style="[
+                {
+                  marginLeft: item.rank * 15 + 'px',
+                  zIndex: item.rank * -1 + 50,
+                },
+              ]"
+              :class="{
+                show: item.show,
+                last: item.lastRank,
+                showchild: item.showChild,
+                open: item.open,
+              }"
+            >
+              <view class="tki-tree-label" @tap="treeItemSelect(item, index)">
+                <text v-if="item.source.type === 1" class="icon icon-zhi"
+                  >直</text
+                >
+                <text v-else class="icon icon-jia">加</text>
+                {{ item.name }}
+              </view>
+              <view
+                v-if="!item.lastRank"
+                @tap.stop="treeItemTap(item, index)"
+                class="tki-tree-icon"
+              >
+                <text
+                  class="iconfont"
+                  :class="[
+                    item.showChild ? 'icon-arrow-down' : 'icon-arrow-up',
+                  ]"
+                />
+              </view>
+            </view>
+          </block>
+        </scroll-view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-import tkiTree from './tki-tree.vue'
-
 export default {
-  components: { tkiTree },
+  name: 'selectMedicalInstitution',
+  props: {
+    range: {
+      type: Array,
+      default: [],
+    },
+    idKey: {
+      type: String,
+      default: 'medicalInstitutionId',
+    },
+    rangeKey: {
+      type: String,
+      default: 'medicalInstitutionSimpleCode',
+    },
+    childrenKey: {
+      type: String,
+      default: 'childMedicalInstitutionList',
+    },
+    title: {
+      type: String,
+      default: '请选择诊所',
+    },
+    openAll: {
+      //默认展开
+      type: Boolean,
+      default: true,
+    },
+    foldAll: {
+      //折叠时关闭所有已经打开的子集，再次打开时需要一级一级打开
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
-      list: [
-        {
-          id: 1,
-          name: '北京市',
-          children: [
-            {
-              id: 11,
-              name: '市辖区',
-              children: [
-                {
-                  id: 111,
-                  name: '西城区',
-                  children: [
-                    {
-                      id: 1111,
-                      name: '南河沿大街',
-                      children: [
-                        {
-                          id: 11111,
-                          name: '紫金宫饭店',
-                          checked: true,
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  id: 112,
-                  name: '东城区',
-                },
-                {
-                  id: 113,
-                  name: '朝阳区',
-                },
-                {
-                  id: 114,
-                  name: '丰台区',
-                },
-              ],
-            },
-          ],
-        },
-      ],
+      showTree: false,
+      treeList: [],
     }
   },
-  onLoad() {},
+  computed: {},
   methods: {
-    // 显示树形选择器
-    showTree() {
-      this.$refs.tkitree._show()
+    show() {
+      this.showTree = true
     },
+    hide() {
+      this.showTree = false
+    },
+    getTreeList() {},
+    //扁平化树结构
+    renderTreeList(list = [], rank = 0, parentId = [], parents = []) {
+      list.forEach((item) => {
+        this.treeList.push({
+          id: item[this.idKey],
+          name: item[this.rangeKey],
+          source: item,
+          parentId, // 父级id数组
+          parents, // 父级id数组
+          rank, // 层级
+          showChild: false, //子级是否显示
+          open: false, //是否打开
+          show: rank === 0, // 自身是否显示
+          hideArr: [],
+        })
+        if (
+          Array.isArray(item[this.childrenKey]) &&
+          item[this.childrenKey].length > 0
+        ) {
+          let parentid = [...parentId],
+            parentArr = [...parents]
+          delete parentArr[this.childrenKey]
+          parentid.push(item[this.idKey])
+          parentArr.push({
+            [this.idKey]: item[this.idKey],
+            [this.rangeKey]: item[this.rangeKey],
+          })
+          this.renderTreeList(
+            item[this.childrenKey],
+            rank + 1,
+            parentid,
+            parentArr,
+          )
+        } else {
+          this.treeList[this.treeList.length - 1].lastRank = true
+        }
+      })
+    },
+    // 默认展开
+    defaultSelect() {
+      if (this.openAll) {
+        this.treeList.forEach((v) => {
+          v.show = true
+          v.showChild = true
+          v.open = true
+        })
+      }
+    },
+    // 点击
+    treeItemTap(item, index) {
+      let list = this.treeList
+      let id = item.id
+      item.showChild = !item.showChild
+      item.open = item.showChild ? true : !item.open
+      list.forEach((childItem, i) => {
+        if (item.showChild === false) {
+          //隐藏所有子级
+          if (!childItem.parentId.includes(id)) {
+            return
+          }
+          if (!this.foldAll) {
+            if (childItem.lastRank !== true && !childItem.open) {
+              childItem.showChild = false
+            }
+            // 为隐藏的内容添加一个标记
+            if (childItem.show) {
+              childItem.hideArr[item.rank] = id
+            }
+          } else {
+            if (childItem.lastRank !== true) {
+              childItem.showChild = false
+            }
+          }
+          childItem.show = false
+        } else {
+          // 打开子集
+          if (childItem.parentId[childItem.parentId.length - 1] === id) {
+            childItem.show = true
+          }
+          // 打开被隐藏的子集
+          if (childItem.parentId.includes(id) && !this.foldAll) {
+            if (childItem.hideArr[item.rank] === id) {
+              childItem.show = true
+              if (childItem.open && childItem.showChild) {
+                childItem.showChild = true
+              } else {
+                childItem.showChild = false
+              }
+              childItem.hideArr[item.rank] = null
+            }
+          }
+        }
+      })
+    },
+    treeItemSelect(item, index) {
+      // 点击选项时触发
+      this.$emit('confirm', item)
+      this.hide()
+    },
+    initTree(range = this.range) {
+      this.treeList = []
+      this.renderTreeList(range)
+      this.$nextTick(() => {
+        this.defaultSelect(range)
+      })
+    },
+  },
+  watch: {
+    range(list) {
+      this.initTree(list)
+    },
+  },
+  mounted() {
+    this.initTree()
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+@import './style.css';
+</style>
--- packages/MP-B/src/pages/login/index.vue
+++ packages/MP-B/src/pages/login/index.vue
@@ -1,38 +1,41 @@
 <template>
-  <div class="content">
-    <img src alt />
-    <div class="login">
-      <van-field
-        left-icon="friends"
-        placeholder="请输入会员名"
-        :value="loginForm.memberCode"
-        @input="loginForm.memberCode = $event.mp.detail"
-      />
-      <van-field
-        left-icon="manager"
-        placeholder="请输入用户名"
-        :value="loginForm.username"
-        @input="loginForm.username = $event.mp.detail"
-      />
-      <van-field
-        type="password"
-        left-icon="lock"
-        placeholder="请输入密码"
-        :value="loginForm.password"
-        @input="loginForm.password = $event.mp.detail"
-      />
-      <div style="margin: 16px;">
-        <van-button round block type="info" @click="onSubmit">登录</van-button>
-      </div>
-    </div>
+  <view>
+    <view class="status_bar"></view>
+    <view class="content">
+      <view class="login">
+        <label>
+          <text class="iconfont icon-institutions" />
+          <input placeholder="请输入会员名" v-model="loginForm.memberCode" />
+        </label>
+        <label>
+          <text class="iconfont icon-user" />
+          <input placeholder="请输入用户名" v-model="loginForm.username" />
+        </label>
+        <label>
+          <text class="iconfont icon-password" />
+          <input
+            password
+            placeholder="请输入密码"
+            v-model="loginForm.username"
+          />
+        </label>
+        <div style="margin: 16px;">
+          <van-button round block type="info" @click="onSubmit"
+            >登录</van-button
+          >
+        </div>
+      </view>
 
-    <selectMedicalInstitution ref="selectMedicalInstitution" />
-  </div>
+      <selectMedicalInstitution
+        ref="selectMedicalInstitution"
+        :memberCode="loginForm.memberCode"
+        @confirm="treeConfirm"
+      ></selectMedicalInstitution>
+    </view>
+  </view>
 </template>
 
 <script>
-import selectMedicalInstitution from '../../components/selectMedicalInstitution/selectMedicalInstitution.vue'
-
 export default {
   data() {
     return {
@@ -46,14 +49,42 @@ export default {
   onLoad() {},
   methods: {
     onSubmit() {
-      this.$refs.selectMedicalInstitution.showTree()
+      this.$refs.selectMedicalInstitution.show()
       console.log('memberCode', this.loginForm.memberCode)
     },
-  },
-  components: {
-    selectMedicalInstitution,
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.status_bar {
+  height: var(--status-bar-height);
+  width: 100%;
+}
+.content {
+  padding: 432rpx 32rpx 0;
+  .login {
+    height: 716rpx;
+    padding: 64rpx 32rpx 0;
+    box-sizing: border-box;
+    background: #feffff;
+    border-radius: 8rpx;
+    box-shadow: 0rpx 20rpx 20rpx 0rpx rgba(0, 0, 0, 0.09);
+    label {
+      display: flex;
+      align-items: center;
+      line-height: 112rpx;
+      border-bottom-width: 1rpx;
+      border-bottom-style: solid;
+      border-bottom-color: rgba(0, 0, 0, 0.15);
+    }
+    text.iconfont {
+      font-size: 40rpx;
+      margin-right: 20rpx;
+    }
+    input {
+      width: 100%;
+    }
+  }
+}
+</style>
