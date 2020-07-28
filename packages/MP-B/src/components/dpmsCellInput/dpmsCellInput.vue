44f77424c0fdfda4d38be98826c6fa367b79a942
jianglang <643546122@qq.com>
2020-07-28 10:16:31 +0800

    feat: 添加dpmsCell, dpmsCellIINput, dpmsCellPicker组件

---
+++ packages/MP-B/src/components/dpmsCellInput/dpmsCellInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <dpmsCell :title="title">
+    <input
+      type="text"
+      :value="value"
+      @input="onChange"
+      :placeholder="placeholder"
+    />
+  </dpmsCell>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    title: String,
+    placeholder: String,
+  },
+  methods: {
+    onChange(e) {
+      this.$emit('input', e.detail.value)
+    },
+  },
+}
+</script>
+
+<style></style>
