9ffae6c78270141d27d477a83574885405cf1c82
jianglang <643546122@qq.com>
2020-07-28 23:53:36 +0800

    feat: 添加公共css和公共路径配置

---
+++ packages/MP-B/vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('APIS', resolve('src/APIS'))
+      .set('views', resolve('src/views'))
+      .set('common', resolve('src/common'))
+  },
+}
