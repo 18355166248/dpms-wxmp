a43a89a6bd1436c165abbc2618161a467afa0acd
任红灯 <hd.ren@medcloud.cn>
2020-07-31 15:25:55 +0800

    feat: 首页部分UI和逻辑调整，今日就诊接口对接

---
+++ packages/MP-B/src/pages/paitent/paitent.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="content">
+    患者资料
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '患者资料',
+    }
+  },
+  onLoad() {},
+  methods: {},
+  components: {},
+}
+</script>
+
+<style></style>
