d24122ed70867f9cb9e1afc20f2306f88cf0ce14
zen <ren.zhang@medcloud.cn>
2020-07-31 17:20:28 +0800

    feat: 首页完成

--- packages/MP-C/src/pages/index/projAptmt.vue
+++ packages/MP-C/src/pages/index/projAptmt.vue
@@ -16,7 +16,14 @@
         <button type="default">按医生预约</button>
       </navigator>
     </view>
-    <view class="filter"> </view>
+    <view class="filter">
+      <dpmsEnumsPicker
+        enumsKey="VisType"
+        v-model="form.visType"
+        title="就诊类型"
+        placeholder="请选择就诊类型"
+      />
+    </view>
   </scroll-view>
 </template>
 
@@ -24,11 +31,43 @@
 export default {
   data() {
     return {
-      title: 'Hello',
+      items: [
+        {
+          value: 'USA',
+          name: '美国',
+        },
+        {
+          value: 'CHN',
+          name: '中国',
+          checked: 'true',
+        },
+        {
+          value: 'BRA',
+          name: '巴西',
+        },
+        {
+          value: 'JPN',
+          name: '日本',
+        },
+        {
+          value: 'ENG',
+          name: '英国',
+        },
+        {
+          value: 'FRA',
+          name: '法国',
+        },
+      ],
+      index: '',
     }
   },
   onLoad() {},
-  methods: {},
+  methods: {
+    bindPickerChange: function (e) {
+      console.log('picker发送选择改变，携带值为', e.target.value)
+      this.index = e.target.value
+    },
+  },
   components: {},
 }
 </script>
