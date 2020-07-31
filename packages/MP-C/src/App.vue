1effc2d48bf3fd0b676acdc6a10cd834d641acfe
zen <ren.zhang@medcloud.cn>
2020-07-31 14:27:56 +0800

    feat: 首页

--- packages/MP-C/src/App.vue
+++ packages/MP-C/src/App.vue
@@ -1,17 +1,52 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+}
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
+@import url('mpcommon/css/common.css');
+
+/*每个页面公共css */
+page {
+  height: 100%;
+}
+
+/*********在线字体代码start*********/
+@font-face {
+  font-family: 'iconfont'; /* project id 1965288 */
+  src: url('//at.alicdn.com/t/font_1965288_g4ckenpgb3s.eot');
+  src: url('//at.alicdn.com/t/font_1965288_g4ckenpgb3s.eot?#iefix')
+      format('embedded-opentype'),
+    url('//at.alicdn.com/t/font_1965288_g4ckenpgb3s.woff2') format('woff2'),
+    url('//at.alicdn.com/t/font_1965288_g4ckenpgb3s.woff') format('woff'),
+    url('//at.alicdn.com/t/font_1965288_g4ckenpgb3s.ttf') format('truetype'),
+    url('//at.alicdn.com/t/font_1965288_g4ckenpgb3s.svg#iconfont') format('svg');
+}
+/*********在线字体代码end*********/
+
+.iconfont {
+  font-family: 'iconfont' !important;
+  font-size: 32rpx;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.icon-location {
+  content: '\e790';
+}
+
+.icon-time {
+  content: '\e784';
+}
 </style>
---
+++ packages/MP-C/src/pages/index/projAptmt.vue
@@ -0,0 +1,51 @@
+<template>
+  <scroll-view class="content" scroll-y>
+    <view class="nav">
+      <navigator
+        class="navigator"
+        url="navigate/navigate?title=navigate"
+        hover-class="navigator-hover"
+      >
+        <button type="default">按预约项目</button>
+      </navigator>
+      <navigator
+        class="navigator"
+        url="navigate/navigate?title=navigate"
+        hover-class="navigator-hover"
+      >
+        <button type="default">按医生预约</button>
+      </navigator>
+    </view>
+    <view class="filter"> </view>
+  </scroll-view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+    }
+  },
+  onLoad() {},
+  methods: {},
+  components: {},
+}
+</script>
+
+<style>
+.content {
+  margin: 0 auto;
+  height: 100%;
+}
+.nav {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 20rpx;
+}
+.navigator {
+  width: 50%;
+}
+</style>
