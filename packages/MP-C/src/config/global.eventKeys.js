495a1fd5ad2819b12647dfe6cb9490fb76fafd47
lishiyan <lishiyan0813@163.com>
2020-08-11 15:12:14 +0800

    feat: 列表刷新

---
+++ packages/MP-C/src/config/global.eventKeys.js
@@ -0,0 +1,10 @@
+// 使用方法参考 https://uniapp.dcloud.io/collocation/frame/communication?id=on
+// 这里配置的是 eventName
+const globalEventKeys = {
+  // 新建/编辑人员 通讯
+  updatePersonFormWithSaveSuccess: 'updatePersonFormWithSaveSuccess',
+  //删除预约
+  deleteAppointFormWithSaveSuccess: 'deleteAppointFormWithSaveSuccess',
+}
+
+export { globalEventKeys }
