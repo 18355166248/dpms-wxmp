eedfe99fab355927cf1c4d215825d89375816298
xiuru <xiuru.yin@medcloud.cn>
2020-08-17 16:55:53 +0800

    feat: 修改http请求，替换emoji表情为空字符

--- packages/MP-B/src/utils/utils.js
+++ packages/MP-B/src/utils/utils.js
@@ -14,7 +14,19 @@ export function omitUndefinedAndNullValue(object) {
       return
     }
 
-    definedMap[key] = value
+    const regEmoji = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/gi
+
+    let regValue = value
+
+    if (typeof regValue !== 'string') {
+      regValue = String(regValue)
+    }
+
+    if (regEmoji.test(regValue)) {
+      definedMap[key] = regValue.replace(regEmoji, '')
+    } else {
+      definedMap[key] = value
+    }
   })
 
   return definedMap
