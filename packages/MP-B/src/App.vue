82a00565078b762437e2802271255844e0c93e8f
shuping.wang <wang0566>
2020-12-09 19:09:36 +0800

    feat: toB小程序新增打电话功能

--- packages/MP-B/src/App.vue
+++ packages/MP-B/src/App.vue
@@ -20,14 +20,13 @@ button::after {
 }
 /*********在线字体代码start*********/
 @font-face {
-  font-family: 'iconfont'; /* project id 1965288 */
-  src: url('//at.alicdn.com/t/font_1965288_7w506cczxy4.eot');
-  src: url('//at.alicdn.com/t/font_1965288_7w506cczxy4.eot?#iefix')
-      format('embedded-opentype'),
-    url('//at.alicdn.com/t/font_1965288_7w506cczxy4.woff2') format('woff2'),
-    url('//at.alicdn.com/t/font_1965288_7w506cczxy4.woff') format('woff'),
-    url('//at.alicdn.com/t/font_1965288_7w506cczxy4.ttf') format('truetype'),
-    url('//at.alicdn.com/t/font_1965288_7w506cczxy4.svg#iconfont') format('svg');
+  font-family: 'iconfont';  /* project id 1965288 */
+  src: url('//at.alicdn.com/t/font_1965288_ur2qolnpvep.eot');
+  src: url('//at.alicdn.com/t/font_1965288_ur2qolnpvep.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_1965288_ur2qolnpvep.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_1965288_ur2qolnpvep.woff') format('woff'),
+  url('//at.alicdn.com/t/font_1965288_ur2qolnpvep.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_1965288_ur2qolnpvep.svg#iconfont') format('svg');
 }
 /*********在线字体代码end*********/
 
@@ -120,6 +119,12 @@ button::after {
 }
 .icon-new-weet::before {
   content: '\e8f0';
+}
+.icon-phone:before {
+  content: '\e698';
+}
+.icon-sms:before {
+  content: '\e60a';
 }
 /* iconfont图标 end */
 </style>
