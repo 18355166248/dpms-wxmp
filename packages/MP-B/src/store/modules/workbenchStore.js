f35ca68a52c9627763acab0423206136ddc511c6
xiubin.li <103432847@qq.com>
2020-12-21 16:39:44 +0800

    feat: 添加预约视图设置

--- packages/MP-B/src/store/modules/workbenchStore.js
+++ packages/MP-B/src/store/modules/workbenchStore.js
@@ -10,6 +10,9 @@ const state = {
     ? getStorage(STORAGE_KEY.MEDICALINSTITUTION)
     : {},
   staff: getStorage(STORAGE_KEY.STAFF) ? getStorage(STORAGE_KEY.STAFF) : {},
+  apptSetting: getStorage(STORAGE_KEY.APPTSETTING)
+    ? getStorage(STORAGE_KEY.APPTSETTING)
+    : {},
 }
 
 const mutations = {
@@ -20,6 +23,10 @@ const mutations = {
   setStaff(state, value) {
     state.staff = value
     setStorage(STORAGE_KEY.STAFF, value)
+  },
+  setApptSetting(state, value) {
+    state.apptSetting = value
+    setStorage(STORAGE_KEY.APPTSETTING, value)
   },
   delMedicalInstitution(state) {
     removeStorage(STORAGE_KEY.MEDICALINSTITUTION)
