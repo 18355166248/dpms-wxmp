34a06910ad6d509bd0b438cc38d3c8a6a539808c
xiubin.li <103432847@qq.com>
2020-12-23 16:59:03 +0800

    feat: 统计详情页

--- packages/MP-B/src/baseSubpackages/statistics/statistics.vue
+++ packages/MP-B/src/baseSubpackages/statistics/statistics.vue
@@ -14,41 +14,41 @@
     />
     <view class="list">
       <view class="tit">新增患者性别分布</view>
-      <view v-show="genderData.length">
+      <view v-if="genderData.length">
         <canvas canvas-id="genderCanvas" id="genderCanvas" class="charts" />
         <view class="link" @click="link(1)">查看全部</view>
       </view>
-      <view v-show="genderData.length === 0" class="empty">
+      <view v-else class="empty">
         暂无统计数据
       </view>
     </view>
     <view class="list">
       <view class="tit">到诊类型分布</view>
-      <view v-show="registerData.length">
+      <view v-if="registerData.length">
         <canvas canvas-id="registerCanvas" id="registerCanvas" class="charts" />
         <view class="link" @click="link(2)">查看全部</view>
       </view>
-      <view v-show="registerData.length === 0" class="empty">
+      <view v-else class="empty">
         暂无统计数据
       </view>
     </view>
     <view class="list">
       <view class="tit">医生收款分布</view>
-      <view v-show="doctorData.length">
+      <view v-if="doctorData.length">
         <canvas canvas-id="doctorCanvas" id="doctorCanvas" class="charts" />
         <view class="link" @click="link(3)">查看全部</view>
       </view>
-      <view v-show="doctorData.length === 0" class="empty">
+      <view v-else class="empty">
         暂无统计数据
       </view>
     </view>
     <view class="list">
       <view class="tit">诊疗项目收款</view>
-      <view v-show="projectData.series.length">
+      <view v-if="projectData.series.length">
         <canvas canvas-id="projectCanvas" id="projectCanvas" class="charts" />
         <view class="link" @click="link(4)">查看全部</view>
       </view>
-      <view v-show="projectData.series.length === 0" class="empty">
+      <view v-else class="empty">
         暂无统计数据
       </view>
     </view>
@@ -56,8 +56,15 @@
 </template>
 
 <script>
+import statisticsAPI from '@/APIS/statistics.api'
 import tabs from '@/components/tabs/tabs.vue'
 import uCharts from '@/components/u-charts/u-charts.min.js'
+
+var _self
+var genderCanvas = null
+var registerCanvas = null
+var doctorCanvas = null
+var projectCanvas = null
 
 export default {
   components: {
@@ -67,55 +74,117 @@ export default {
     return {
       current: 0,
       tabs: [
-        { name: '昨天', val: 111 },
-        { name: '近七天', val: 222 },
-        { name: '近30天', val: 333 },
+        { name: '昨天', val: 1 },
+        { name: '近七天', val: 7 },
+        { name: '近30天', val: 30 },
       ],
       cWidth: '',
       cHeight: '',
       pixelRatio: 1,
-      genderData: [
-        {
-          name: '其他',
-          data: 50,
-        },
-        {
-          name: '男',
-          data: 30,
-        },
-        {
-          name: '女',
-          data: 20,
-        },
-      ],
+      genderData: [],
       registerData: [],
       doctorData: [],
       projectData: {
-        categories: ['2012', '2013', '2014', '2015', '2016', '2017'],
-        series: [15, 20, 45, 37, 43, 34],
+        categories: [],
+        series: [],
       },
     }
   },
   onLoad() {
+    _self = this
     this.cWidth = uni.upx2px(750)
     this.cHeight = uni.upx2px(608)
-    this.showPie('genderCanvas', this.genderData)
-    this.showPie('registerCanvas', this.registerData)
-    this.showRing('doctorCanvas', this.doctorData)
-    this.showColumn('projectCanvas', this.projectData)
+    this.setCharts()
+    this.getStatistics(0)
   },
   methods: {
     changeTab(i) {
-      console.log('当前选中的值：' + this.tabs[i].val)
+      this.getStatistics(i)
     },
-    showPie(canvasId, chartData) {
-      new uCharts({
+    getStatistics(i) {
+      statisticsAPI
+        .getStatistics({
+          type: this.tabs[i].val,
+        })
+        .then((res) => {
+          const { data } = res
+
+          if (data.patient.reduce((acc, val) => acc + val.count, 0)) {
+            this.genderData = data.patient.map((v) => {
+              return {
+                name: v.product,
+                data: v.count,
+                format(val) {
+                  return v.count + '人 ' + val + '%'
+                },
+              }
+            })
+          } else {
+            this.genderData = []
+          }
+
+          if (data.register.reduce((acc, val) => acc + val.count, 0)) {
+            this.registerData = data.register.map((v) => {
+              return {
+                name: v.product,
+                data: v.count,
+                format(val) {
+                  return v.count + '人 ' + val + '%'
+                },
+              }
+            })
+          } else {
+            this.registerData = []
+          }
+
+          this.doctorData = data.doctor.map((v) => {
+            return {
+              name: v.product,
+              data: v.count,
+              format(val) {
+                return _self.$utils.formatPrice(val)
+              },
+            }
+          })
+
+          this.projectData.categories = data.project.map((v) => v.product)
+          this.projectData.series = data.project.map((v) => v.count)
+
+          this.changeData()
+        })
+        .catch()
+    },
+    changeData() {
+      genderCanvas.updateData({
+        series: this.genderData,
+      })
+      registerCanvas.updateData({
+        series: this.registerData,
+      })
+      doctorCanvas.updateData({
+        series: this.doctorData,
+      })
+      projectCanvas.updateData({
+        categories: this.projectData.categories,
+        series: [
+          {
+            name: '',
+            data: this.projectData.series,
+            format(val) {
+              return _self.$utils.formatPrice(val)
+            },
+          },
+        ],
+      })
+    },
+    setCharts() {
+      genderCanvas = new uCharts({
         $this: this,
-        canvasId: canvasId,
+        canvasId: 'genderCanvas',
         type: 'pie',
         legend: { show: true },
         pixelRatio: this.pixelRatio,
-        series: chartData,
+        series: [],
         width: this.cWidth * this.pixelRatio,
         height: this.cHeight * this.pixelRatio,
         dataLabel: true,
@@ -125,15 +194,29 @@ export default {
           },
         },
       })
-    },
-    showRing(canvasId, chartData) {
-      new uCharts({
+      registerCanvas = new uCharts({
         $this: this,
-        canvasId: canvasId,
+        canvasId: 'registerCanvas',
+        type: 'pie',
+        legend: { show: true },
+        pixelRatio: this.pixelRatio,
+        series: [],
+        width: this.cWidth * this.pixelRatio,
+        height: this.cHeight * this.pixelRatio,
+        dataLabel: true,
+        extra: {
+          pie: {
+            lableWidth: 15,
+          },
+        },
+      })
+      doctorCanvas = new uCharts({
+        $this: this,
+        canvasId: 'doctorCanvas',
         type: 'ring',
         legend: { show: true },
         pixelRatio: this.pixelRatio,
-        series: chartData,
+        series: [],
         width: this.cWidth * this.pixelRatio,
         height: this.cHeight * this.pixelRatio,
         dataLabel: true,
@@ -145,20 +228,18 @@ export default {
           },
         },
       })
-    },
-    showColumn(canvasId, chartData) {
-      new uCharts({
+      projectCanvas = new uCharts({
         $this: this,
-        canvasId: canvasId,
+        canvasId: 'projectCanvas',
         type: 'column',
         legend: { show: false },
         padding: [30, 15, 20, 15],
         pixelRatio: this.pixelRatio,
-        categories: chartData.categories,
+        categories: [],
         series: [
           {
             name: '',
-            data: chartData.series,
+            data: [],
           },
         ],
         xAxis: {
@@ -175,19 +256,19 @@ export default {
             type: 'group',
             width:
               (this.cWidth * this.pixelRatio * 0.45) /
-              chartData.categories.length,
+              this.projectData.categories.length,
           },
         },
       })
     },
     link(type) {
-      console.log(111)
-      this.$utils.push({
+      uni.navigateTo({
         url:
           '/baseSubpackages/statistics/detail/detail?type=' +
           type +
           '&time=' +
           this.tabs[this.current].val,
+        animationType: 'slide-in-right',
       })
     },
   },
