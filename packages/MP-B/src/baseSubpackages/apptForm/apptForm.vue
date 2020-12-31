feb6482aa23d5436e874532ff505de74a18b7c66
zen <ren.zhang@medcloud.cn>
2020-12-31 15:16:18 +0800

    fix: 报表bug修改

--- packages/MP-B/src/baseSubpackages/apptForm/apptForm.vue
+++ packages/MP-B/src/baseSubpackages/apptForm/apptForm.vue
@@ -774,23 +774,25 @@ export default {
             appt: { ...formatValue, ...res.data },
           })
 
-          if (type === 'createRegister' || type === 'editRegister') {
-            this.$utils.push({
-              url: '/baseSubpackages/todayWork/todayWork',
-            })
-          }
-          if (type === 'createAppt') {
-            this.$utils.push({
-              url: '/baseSubpackages/apptView/apptView',
-            })
-          }
-          if (type === 'editAppt') {
-            this.$utils.push({
-              url:
-                '/baseSubpackages/apptForm/apptDetail?appointmentId=' +
-                this.paramsObj.appointmentId,
-            })
-          }
+          this.$utils.back()
+
+          // if (type === 'createRegister' || type === 'editRegister') {
+          //   this.$utils.push({
+          //     url: '/baseSubpackages/todayWork/todayWork',
+          //   })
+          // }
+          // if (type === 'createAppt') {
+          //   this.$utils.push({
+          //     url: '/baseSubpackages/apptView/apptView',
+          //   })
+          // }
+          // if (type === 'editAppt') {
+          //   this.$utils.push({
+          //     url:
+          //       '/baseSubpackages/apptForm/apptDetail?appointmentId=' +
+          //       this.paramsObj.appointmentId,
+          //   })
+          // }
         })
         .finally(() => {
           this.saveLoading = false
