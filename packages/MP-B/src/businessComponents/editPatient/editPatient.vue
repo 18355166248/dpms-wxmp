6088385fd7a2bf492a30971aded21fdaad42d899
zen <ren.zhang@medcloud.cn>
2021-03-08 15:54:10 +0800

    feat: 新建患者 age 生日

--- packages/MP-B/src/businessComponents/editPatient/editPatient.vue
+++ packages/MP-B/src/businessComponents/editPatient/editPatient.vue
@@ -224,6 +224,10 @@ export default {
           pattern: /^(?:[1-9][0-9]?|1[01][0-9]|140)$/,
           message: '年龄不合法',
         },
+        certificatesNo: {
+          pattern: /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/,
+          message: '身份证不合法',
+        },
         contactLabel: {
           required: true,
           message: '请选择联系电话标签',
