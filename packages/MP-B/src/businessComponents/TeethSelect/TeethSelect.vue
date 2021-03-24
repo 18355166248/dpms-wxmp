5ae8286b865d91e3add04cedd8b65e707bc873f0
wei.zhou <wei.zhou@medcloud.cn>
2021-03-24 15:46:52 +0800

    fix: 牙位选择器顺序

--- packages/MP-B/src/businessComponents/TeethSelect/TeethSelect.vue
+++ packages/MP-B/src/businessComponents/TeethSelect/TeethSelect.vue
@@ -2,9 +2,9 @@
   <div class="teeth-select" :style="style" @click="openSelector">
     <div v-for="(yas, i) in computedValue" :key="i">
       <span v-for="ya in yas" :key="ya.label">
-        {{ya.label}}
+        {{ ya.label }}
         <span class="sup">
-          <span v-for="a in ya.area" :key="a">{{a}}</span>
+          <span v-for="a in ya.area" :key="a">{{ a }}</span>
         </span>
       </span>
     </div>
@@ -19,36 +19,46 @@ export default {
     value: {
       type: Object,
       default() {
-        return {teeth: {}}
-      }
+        return { teeth: {} }
+      },
     },
     index: Number,
     disabled: Boolean,
   },
   data() {
     return {
-      dataValue: this.value || {teeth: {}}
+      dataValue: this.value || { teeth: {} },
     }
   },
   computed: {
     computedValue() {
       const teeth = this.dataValue.teeth
-      const value = Object.keys(teeth).reduce((r, k) => {
-        const quadrant = k[0] % 4 || 4
-        r[quadrant - 1].push({label: k[0] > 4 ? ru[k[1] - 1] : k[1], area: Object.keys(teeth[k] || {})})
-        return r
-      }, Array.from({length: 4}, () => []))
+      const value = Object.keys(teeth).reduce(
+        (r, k) => {
+          const quadrant = k[0] % 4 || 4
+          r[quadrant - 1][[1, 4].includes(quadrant) ? 'unshift' : 'push']({
+            label: k[0] > 4 ? ru[k[1] - 1] : k[1],
+            area: Object.keys(teeth[k] || {}),
+          })
+          return r
+        },
+        Array.from({ length: 4 }, () => []),
+      )
       value.splice(2, 0, value[3])
       return value.slice(0, 4)
-    }
+    },
   },
   methods: {
     openSelector() {
       if (this.disabled) return
-      this.$utils.push({url: `/businessComponents/TeethSelect/Selector?value=${JSON.stringify(this.dataValue)}&uid=${this._uid}`})
+      this.$utils.push({
+        url: `/businessComponents/TeethSelect/Selector?value=${JSON.stringify(
+          this.dataValue,
+        )}&uid=${this._uid}`,
+      })
     },
     onChange() {
-      uni.$on(`teethSelectChange${this._uid}`, result => {
+      uni.$on(`teethSelectChange${this._uid}`, (result) => {
         this.dataValue = result
         this.$emit('input', this.dataValue, this.index)
       })
@@ -60,21 +70,21 @@ export default {
   watch: {
     value(newVal, oldVal) {
       if (newVal === oldVal) return
-      this.dataValue = this.value || {teeth: {}}
-    }
-  }
+      this.dataValue = this.value || { teeth: {} }
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.teeth-select{
+.teeth-select {
   color: #5cbb89;
   display: flex;
   flex-wrap: wrap;
   height: 102rpx;
   line-height: 51rpx;
   font-size: 22rpx;
-  >div{
+  > div {
     border-bottom: solid 2rpx #5cbb89;
     border-right: solid 2rpx #5cbb89;
     width: 50%;
@@ -82,24 +92,25 @@ export default {
     box-sizing: border-box;
     overflow: hidden;
     white-space: nowrap;
-    &:nth-child(2n){
+    &:nth-child(2n) {
       border-right: none;
       padding-left: 14rpx;
-      >span{
+      > span {
         margin-right: 8rpx;
       }
     }
-    &:nth-child(n+3){
+    &:nth-child(n + 3) {
       border-bottom: none;
     }
-    &:nth-child(2n+1){
-      text-align: right;
-      padding-right: 14rpx;
-      >span{
+    &:nth-child(2n + 1) {
+      display: flex;
+      justify-content: flex-end;
+      padding-right: 12rpx;
+      > span {
         margin-left: 8rpx;
       }
     }
-    .sup{
+    .sup {
       position: relative;
       top: -6rpx;
       margin: 0 0 0 2rpx;
