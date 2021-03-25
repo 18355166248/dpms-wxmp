62c62e4a363b4902ecaac451b9e4ddf9dbae3dfc
zen <ren.zhang@medcloud.cn>
2021-03-25 15:31:07 +0800

    feat: show loading

--- packages/MP-B/src/baseSubpackages/todayWork/todayWork.vue
+++ packages/MP-B/src/baseSubpackages/todayWork/todayWork.vue
@@ -818,6 +818,7 @@ export default {
         if (listErr) {
           this.dataSourceStatus.request = 'error'
           this.dataSourceStatus.status = 'more'
+          uni.hideLoading()
         } else if (listRes) {
           this.dataSourceStatus.request = 'success'
 
