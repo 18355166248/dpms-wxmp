c47052cb0210583ecc7e27c01b993e5bf1846eac
zen <ren.zhang@medcloud.cn>
2021-04-21 17:46:03 +0800

    feat: 表格组件

---
+++ packages/MP-B/src/pages/achievement/doctor.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="ach_doc">
+    <dpmsTable />
+  </view>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {},
+  destroyed() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.ach_doc {
+  width: 100%;
+}
+</style>
