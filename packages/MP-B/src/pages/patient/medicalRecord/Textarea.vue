f549268caa49678f97c9d56ce097963ae4c58e7c
wei.zhou <wei.zhou@medcloud.cn>
2021-04-22 20:40:10 +0800

    feat: 修改词条前缀

--- packages/MP-B/src/pages/patient/medicalRecord/Textarea.vue
+++ packages/MP-B/src/pages/patient/medicalRecord/Textarea.vue
@@ -82,7 +82,13 @@ const keyPs = [
     prefix: 'presentIllnessHistory',
   },
   { key: 'doctorAdvice', func: 'getDoctorAdviceDict', prefix: 'doctorAdvice' },
-  { key: 'dispose', func: 'getDisposeDict', prefix: 'disposal' }, //处置
+  {
+    //处置
+    key: 'dispose',
+    func: 'getDisposeDict',
+    prefix: 'disposal',
+    prefixDict: 'dispose',
+  },
   { key: 'diagnosisDesc', prefix: 'diagnosis' },
   {
     key: 'pastIllnessHistory',
@@ -94,12 +100,14 @@ const keyPs = [
     key: 'checkNormalSymptoms',
     func: 'getExaminationDict',
     prefix: 'dentalExamination',
+    prefixDict: 'examination',
   },
   {
     //辅助检查
     key: 'checkRaySymptoms',
     func: 'getExaminationDict',
     prefix: 'auxiliaryExamination',
+    prefixDict: 'examination',
   },
   {
     key: 'treatmentProgram',
@@ -146,7 +154,7 @@ export default {
             (r, k) => ({
               ...r,
               [k
-                .replace(RegExp(kp.prefix, 'i'), '')
+                .replace(RegExp(kp.prefixDict || kp.prefix, 'i'), '')
                 .replace('DictVOS', 'DictVO')]: Array.isArray(a[k])
                 ? replaceKey(a[k], key)
                 : a[k],
