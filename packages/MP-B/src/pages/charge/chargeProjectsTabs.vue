e5ac711b82dabbff134d8e10b786e948cd461407
肖青青 <xiaoqingqing@medcloud.cn>
2021-05-16 15:07:40 +0800

    feat:增加明细收费和处置收费选择项、新建明细收费和处置收费项目选择页面

---
+++ packages/MP-B/src/pages/charge/chargeProjectsTabs.vue
@@ -0,0 +1,87 @@
+<template>
+  <view class="search-charge-wrap">
+    <view>
+      <!--tab选项-->
+      <tabs
+        :value="currentTab"
+        :scroll="false"
+        :tabs="tabList"
+        field="name"
+        height="96rpx"
+        lineHeight="4rpx"
+        activeColor="#5CBB89"
+        lineColor="#5CBB89"
+        lineScale="0.2"
+        @change="changeTab"
+      />
+      <!--搜索-->
+      <searchInput></searchInput>
+      <!--tab内容-->
+      <view class="main-container">
+
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+import billAPI from '@/APIS/bill/bill.api'
+import searchInput from './common/searchInput'
+import { mapMutations, mapState } from 'vuex'
+
+export default {
+  name: '',
+  data() {
+    return {
+      currentTab:0,
+      tabList: [
+        { name: '诊疗服务', val: 0 },
+        { name: '套餐项目', val: 1 },
+        { name: '销售商品', val: 2 },
+      ],
+    }
+  },
+  computed: {
+    ...mapState('searchProjectStore', ['searchProjectList']),
+    ...mapState('dispose', ['disposeList']),
+    ...mapState('checkstand', ['chargeType', 'itemType']),
+  },
+  onLoad() {
+
+  },
+  onShow() {
+
+  },
+  onHide() {},
+  onUnload() {},
+  methods: {
+    ...mapMutations('dispose', ['setDisposeList', 'setReceivableAmount']),
+    changeTab(i){
+      this.currentTab = this.tabList[i].val
+    }
+  },
+  watch: {},
+  components: { searchInput },
+}
+</script>
+<style lang="scss" scoped>
+.ellipsisChargeName {
+  max-width: 560rpx;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+}
+
+.search-charge-wrap {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  box-sizing: border-box;
+  //padding-bottom: constant(safe-area-inset-bottom);
+  //padding-bottom: env(safe-area-inset-bottom);
+
+}
+</style>
--- packages/MP-B/src/store/modules/checkstand.js
+++ packages/MP-B/src/store/modules/checkstand.js
@@ -6,6 +6,7 @@
 //   EXCLUSIVE_CARD_BILL(5, '专享卡交易', ''),
 //   // 处置收费
 //   DISPOSAL_BILL(6, '普通收费', '');
+//   明细收费1
 const state = {
   chargeType: 1,
   billType: 2,
@@ -15,6 +16,14 @@ const typeDic = {
   1: {
     billType: 2,
     itemType: 12,
+  },
+  2: {
+    billType: 1,
+    itemType: 5,
+  },
+  3: {
+    billType: 6,
+    itemType: 5,
   },
 }
 const mutations = {
