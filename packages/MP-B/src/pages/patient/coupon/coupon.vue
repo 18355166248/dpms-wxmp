061d1af6e82c4a9b6d4c145009fece4d1c6468b7
欧阳强 <yangqiang.ou@medcloud.cn>
2021-06-19 15:43:58 +0800

    feat: btn底部

--- packages/MP-B/src/pages/patient/coupon/coupon.vue
+++ packages/MP-B/src/pages/patient/coupon/coupon.vue
@@ -380,8 +380,8 @@ export default {
     background-color: #fff;
     padding: 16rpx 32rpx;
     padding-bottom: 16rpx;
-    padding-bottom: calc(16rpx + constant(safe-area-inset-bottom)); /* no */
-    padding-bottom: calc(16rpx + env(safe-area-inset-bottom)); /* no */
+    padding-bottom: calc(8px + constant(safe-area-inset-bottom)); /* no */
+    padding-bottom: calc(8px + env(safe-area-inset-bottom)); /* no */
     .ensurebutton {
       height: 80rpx;
       background: #5cbb89;
