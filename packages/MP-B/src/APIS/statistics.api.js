d073377bba22239761df270e28d37dd6b3fbc580
胡程 <383686357@qq.com>
2021-06-24 11:00:34 +0800

    feat: 添加往特定网址post vue的错误信息

--- packages/MP-B/src/APIS/statistics.api.js
+++ packages/MP-B/src/APIS/statistics.api.js
@@ -1,9 +1,17 @@
 import httper from './http'
+import httperStatistic from './httpStatistic'
 
 const statisticsAPI = {
   // 获取诊所运营统计
   getStatistics(params) {
     return httper.get('diagnosis/mini-apps/statistics/statistics', params)
+  },
+  log(data) {
+    return httperStatistic.post('https://www.baidu.com', data, {
+      headers: {
+        'content-type': 'application/json; charset=utf-8',
+      },
+    })
   },
 }
 
