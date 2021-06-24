cddaed6429b587b8343fbba2d290af97cb5fd031
肖洋 <yang.xiao@medcloud.cn>
2021-06-24 16:59:14 +0800

    perf: 点击查看病例新增辨识参数

--- packages/MP-B/src/baseSubpackages/approvalManagement/components/requestApplied.vue
+++ packages/MP-B/src/baseSubpackages/approvalManagement/components/requestApplied.vue
@@ -116,7 +116,7 @@ export default {
     showDetail(item) {
       this.medicalRecordId = item.businessId
       wx.navigateTo({
-        url: `/pages/patient/medicalRecord/detail?medicalRecordId=${this.medicalRecordId}`,
+        url: `/pages/patient/medicalRecord/detail?medicalRecordId=${this.medicalRecordId}&checkMedRecord=false`,
       })
     },
     getApprovalDetail() {
--- packages/MP-B/src/pages/patient/medicalRecord/detail.vue
+++ packages/MP-B/src/pages/patient/medicalRecord/detail.vue
@@ -147,7 +147,12 @@
       </div>
     </div>
     <div class="bottom" v-if="currentStaffApproveType === 0">
-      <div v-if="detail.approveStatus === 3 || !detail.approveStatus">
+      <div
+        v-if="
+          (detail.approveStatus === 3 || !detail.approveStatus) &&
+          checkMedRecord
+        "
+      >
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
@@ -161,6 +166,7 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
+      <div v-if="detail.approveStatus === 3 && checkMedRecord"></div>
     </div>
     <div class="bottom" v-if="currentStaffApproveType === 2">
       <div v-if="detail.approveStatus === 1 || detail.approveStatus === 4">
@@ -173,6 +179,7 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
+      <div v-if="detail.approveStatus === 3 && checkMedRecord"></div>
     </div>
     <div class="bottom" v-if="currentStaffApproveType === 3">
       <div v-if="detail.approveStatus === 1 || detail.approveStatus === 4">
@@ -187,6 +194,7 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
+      <div v-if="detail.approveStatus === 3 && checkMedRecord"></div>
     </div>
   </div>
 </template>
@@ -203,6 +211,7 @@ export default {
     return {
       currentStaffApproveType: 0,
       detail: {},
+      checkMedRecord: true, //审批管理跳转过来参数
       approveRemark: '',
       visTypeMap: this.$dpmsUtils.getEnums('VisType').properties,
       TreatmentTypes: [], // 就诊类型
@@ -321,10 +330,14 @@ export default {
       this.approveRemark = ev.target.value
     },
   },
-  onLoad({ medicalRecordId, patientId }) {
+  onLoad({ medicalRecordId, patientId, checkMedRecord }) {
     this.medicalRecordId = medicalRecordId
     this.patientId = patientId
+    this.checkMedRecord = checkMedRecord
     this.initTreatmentTypes()
+    if (checkMedRecord === 'false') {
+      this.checkMedRecord = false
+    }
   },
   onShow() {
     this.getRole()
