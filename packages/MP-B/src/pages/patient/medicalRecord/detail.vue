bf89a5ded4800f7a98c21fc17816cbbeedc640cb
肖洋 <yang.xiao@medcloud.cn>
2021-07-09 02:01:22 +0800

    perf: 审批管理权限修改

--- packages/MP-B/src/pages/patient/medicalRecord/detail.vue
+++ packages/MP-B/src/pages/patient/medicalRecord/detail.vue
@@ -155,6 +155,13 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
+      <div v-if="detail.approveStatus === 2">
+        <button @click="noPassing(detail)">不 通 过</button>
+        <button @click="passing(detail)">通 过</button>
+      </div>
+      <div v-if="detail.approveStatus === 1 || detail.approveStatus === 4">
+        <button @click="againEdit(detail)">重 新 修 改</button>
+      </div>
     </div>
     <div class="bottom" v-if="currentStaffApproveType === 1">
       <div v-if="detail.approveStatus === 2">
@@ -165,7 +172,6 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
-      <div v-if="detail.approveStatus === 3"></div>
     </div>
     <div class="bottom" v-if="currentStaffApproveType === 2">
       <div v-if="detail.approveStatus === 1 || detail.approveStatus === 4">
@@ -178,7 +184,6 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
-      <div v-if="detail.approveStatus === 3"></div>
     </div>
     <div class="bottom" v-if="currentStaffApproveType === 3">
       <div v-if="detail.approveStatus === 1 || detail.approveStatus === 4">
@@ -193,7 +198,6 @@
         <button @click="deleteMedicalRecord">删 除</button>
         <button @click="toEdit">编 辑</button>
       </div>
-      <div v-if="detail.approveStatus === 3"></div>
     </div>
   </div>
 </template>
