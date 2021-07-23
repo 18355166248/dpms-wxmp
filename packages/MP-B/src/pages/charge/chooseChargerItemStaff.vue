f084c599a1fa43610ca6e906095cad21fbda26f0
yechengcheng <chengcheng.ye@medcloud.cn>
2021-07-23 13:27:06 +0800

    fix: 收费重复点击

--- packages/MP-B/src/pages/charge/chooseChargerItemStaff.vue
+++ packages/MP-B/src/pages/charge/chooseChargerItemStaff.vue
@@ -103,20 +103,8 @@ export default {
         }
       })
 
-      // 默认选第一个
-      // if(!item.salesList && this.type === 'doctor') {
-      //   id = this.List[1].staffId
-      // }
-
       this.$set(item, this.itemStaffId, id)
     })
-
-
-    // 默认选第一个
-    if(!this.staffId && this.type === 'doctor') {
-      this.staffId = this.List[1].staffId
-    }
-
   },
   onShow() {
     if (typeof this.staffId == 'undefined') {
